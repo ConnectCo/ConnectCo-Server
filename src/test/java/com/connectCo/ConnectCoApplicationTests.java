0f14358f610c7aa8e6ac7b7bef6567585eed507d
준써크 <kjs8960@inha.edu>
2023-11-08 00:12:22 +0900

    init commit

---
+++ src/test/java/com/connectCo/ConnectCoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connectCo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectCoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
