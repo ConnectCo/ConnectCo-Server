2b92b33afaf2f15f6d43fb9cb1acaa50c0b6db00
준써크 <junsuk8960@naver.com>
2023-11-14 17:06:22 +0900

    chore: create swagger configuration

---
+++ src/main/java/com/connectCo/global/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.connectCo.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Swagger for ConnectCo")
+                .description("Springdoc을 사용한 Swagger UI for ConnectCo")
+                .version("1.0.0");
+    }
+}
