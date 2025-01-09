d87fa9e1e5d1ce29b84480ff56c08821bcf365b7
hig1002 <hig7463@naver.com>
2025-01-09 21:17:44 +0900

    feat: ChatRoom 엔티티 생성

--- src/main/java/com/connectCo/domain/chat/entity/Chat.java
+++ src/main/java/com/connectCo/domain/chat/entity/Chat.java
@@ -7,6 +7,7 @@ import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 @Entity
 @Builder
 public class Chat {
--- src/main/java/com/connectCo/domain/chat/entity/ChatRoom.java
+++ src/main/java/com/connectCo/domain/chat/entity/ChatRoom.java
@@ -1,4 +1,22 @@
 package com.connectCo.domain.chat.entity;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Entity
+@Builder
 public class ChatRoom {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
 }
