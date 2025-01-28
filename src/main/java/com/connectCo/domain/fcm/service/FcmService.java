523d2fff682443874517879075a61ddce8dd2420
hig1002 <hig7463@naver.com>
2025-01-28 14:31:41 +0900

    fix: fireBaseConfig 오류 수정

--- src/main/java/com/connectCo/domain/fcm/service/FcmService.java
+++ src/main/java/com/connectCo/domain/fcm/service/FcmService.java
@@ -1,7 +1,5 @@
 package com.connectCo.domain.fcm.service;
 
-import com.connectCo.domain.Member.entity.Member;
-import com.connectCo.domain.Member.repository.MemberRepository;
 import com.connectCo.domain.fcm.mapper.FcmMapper;
 import com.connectCo.global.exception.CustomApiException;
 import com.connectCo.global.exception.ErrorCode;
@@ -9,21 +7,21 @@ import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.stereotype.Service;
-
-import java.io.IOException;
 
 @Service
 @RequiredArgsConstructor
+@DependsOn("firebaseConfig")
 public class FcmService {
 
     private final FcmMapper fcmMapper;
 
-    public String sendPushNotification(String targetToken, String title, String body) {
+    public void sendPushNotification(String targetToken, String title, String body) {
 
         try {
             Message message = fcmMapper.ToMessage(targetToken, title, body);//fcm 메세지 객체 빌드
-            return FirebaseMessaging.getInstance().send(message);
+            FirebaseMessaging.getInstance().send(message);
         } catch (FirebaseMessagingException e) {
             throw new CustomApiException(ErrorCode.FIREBASE_PUSH_FAILED);
         }
