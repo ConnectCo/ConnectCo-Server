db7d8a61389bcb753b9b76c29946c331ce3594ca
junseokkim <junsuk8960@naver.com>
2025-03-03 17:21:20 +0900

    fix: 구글 로그인 방식 수정

--- src/main/java/com/connectCo/domain/member/client/GoogleMemberClient.java
+++ src/main/java/com/connectCo/domain/member/client/GoogleMemberClient.java
@@ -1,31 +1,18 @@
 package com.connectCo.domain.member.client;
 
-import com.connectCo.domain.member.dto.client.GoogleMemberResponse;
-import com.connectCo.global.exception.CustomApiException;
-import com.connectCo.global.exception.ErrorCode;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @Component
 public class GoogleMemberClient {
-    private final WebClient webClient;
 
-    public GoogleMemberClient(WebClient.Builder webClientBuilder) {
-        this.webClient = webClientBuilder
-                .baseUrl("https://www.googleapis.com/oauth2/v3/userinfo")
-                .build();
-    }
-
-    public String getGoogleUserId(String accessToken) {
-        GoogleMemberResponse response = webClient
-                .get()
-                .header("Authorization", "Bearer " + accessToken)
-                .retrieve()
-                .bodyToMono(GoogleMemberResponse.class)
-                .block();
-        if(response != null) {
-            return response.getSub();
+    public String getGoogleUserId(String idToken) {
+        try {
+            DecodedJWT decodedJWT = JWT.decode(idToken);
+            return decodedJWT.getClaim("sub").asString(); // 사용자 고유 ID 반환
+        } catch (Exception e) {
+            throw new RuntimeException("Invalid Google ID Token", e);
         }
-        throw new CustomApiException(ErrorCode.INVALID_GOOGLE_TOKEN);
     }
 }
