87002b0e0c467b37c76b7151ccc0ba9ea6213258
junseokkim <junsuk8960@naver.com>
2025-03-03 19:56:13 +0900

    fix: 구글 및 카카오 토큰 검증 로직 추가

--- src/main/java/com/connectCo/domain/member/client/GoogleMemberClient.java
+++ src/main/java/com/connectCo/domain/member/client/GoogleMemberClient.java
@@ -1,18 +1,31 @@
 package com.connectCo.domain.member.client;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.interfaces.DecodedJWT;
+import com.connectCo.domain.member.dto.client.GoogleMemberResponse;
+import com.connectCo.global.exception.CustomApiException;
+import com.connectCo.global.exception.ErrorCode;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Component
 public class GoogleMemberClient {
 
+    private final WebClient webClient;
+
+    public GoogleMemberClient(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.build();
+    }
+
     public String getGoogleUserId(String idToken) {
-        try {
-            DecodedJWT decodedJWT = JWT.decode(idToken);
-            return decodedJWT.getClaim("sub").asString(); // 사용자 고유 ID 반환
-        } catch (Exception e) {
-            throw new RuntimeException("Invalid Google ID Token", e);
+        GoogleMemberResponse response = webClient.get()
+            .uri("https://oauth2.googleapis.com/tokeninfo?id_token=" + idToken)
+            .retrieve()
+            .bodyToMono(GoogleMemberResponse.class)
+            .block();
+
+        if (response != null) {
+            return response.getSub(); // 사용자 고유 ID 반환
         }
+
+        throw new CustomApiException(ErrorCode.INVALID_GOOGLE_TOKEN);
     }
 }
