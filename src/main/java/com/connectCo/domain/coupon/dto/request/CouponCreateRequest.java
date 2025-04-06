36eabcef472d3e82058ff14baccb70d84b7739bb
hig1002 <hig7463@naver.com>
2025-04-06 17:13:58 +0900

    fix: 쿠폰 생성 api 오류 수정

--- src/main/java/com/connectCo/domain/coupon/dto/request/CouponCreateRequest.java
+++ src/main/java/com/connectCo/domain/coupon/dto/request/CouponCreateRequest.java
@@ -1,9 +1,8 @@
 package com.connectCo.domain.coupon.dto.request;
 
-import com.connectCo.global.validation.annotation.ExistStore;
 import jakarta.validation.constraints.NotBlank;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,7 +14,7 @@ public class CouponCreateRequest {
     @NotBlank(message = "쿠폰 이름은 필수 입력값입니다.")
     private String name;
 
-    @NotBlank(message = "쿠폰 신청 마감일은 필수 입력값입니다.")
+    @NotNull(message = "쿠폰 신청 마감일은 필수 입력값입니다.")
     private LocalDate expiredAt;
 
     @NotBlank(message = "쿠폰 세부 설명은 필수 입력값입니다.")
